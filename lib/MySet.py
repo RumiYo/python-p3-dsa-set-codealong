cc9354edb4159a36d04862b62c465dfecdda745f
RumiYo <yokoyamarumi@gmail.com>
2024-04-07 16:53:08 -0400

    Complete lab

--- lib/MySet.py
+++ lib/MySet.py
@@ -8,6 +8,12 @@ class MySet:
     def has(self, value):
         # return self.dictionary[value]
         return value in self.dictionary
+    
+    def __str__(self):
+        set_list = []
+        for key, value in self.dictionary.items():
+            set_list.append(str(key))
+        return f'MySet: {{{",".join(set_list)}}}'
 
     def add(self, value):
         self.dictionary[value] = True
